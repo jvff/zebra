2779c8068d1c79028d184785aafcea06c9a5ccf7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-05-24 17:08:09 -0300

    Reword security validation description
    Make the security impact clearer and in a separate section.
    
    Co-authored-by: teor <teor@riseup.net>

--- zebra-network/src/peer_set/candidate_set.rs
+++ zebra-network/src/peer_set/candidate_set.rs
@@ -333,9 +333,11 @@ where
 /// - modify the address data, or
 /// - delete the address.
 ///
-/// Currently, this method will offset the reported `last_seen` time to prevent clock skews
-/// from causing the peers to be placed too far back or in the front of the reconnection queue
-/// incorrectly.
+/// # Security
+///
+/// Adjusts untrusted last seen times so they are not in the future. This stops
+/// malicious peers keeping all their addresses at the front of the connection
+/// queue. Honest peers with future clock skew also get adjusted.
 fn validate_addrs(
     addrs: impl IntoIterator<Item = MetaAddr>,
     last_seen_limit: DateTime<Utc>,
