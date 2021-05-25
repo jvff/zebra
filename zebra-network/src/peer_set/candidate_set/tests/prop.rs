5e189d2d48a36b43896817c06be85354ce34fb21
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-05-25 00:53:10 +0000

    Make the `last_seen_limit` an arbitrary property
    It was previously using the current time, but the test is broader with
    an arbitrary date.

--- zebra-network/src/peer_set/candidate_set/tests/prop.rs
+++ zebra-network/src/peer_set/candidate_set/tests/prop.rs
@@ -1,5 +1,6 @@
-use chrono::Utc;
 use proptest::{collection::vec, prelude::*};
+
+use zebra_chain::serialization::arbitrary::datetime_full;
 
 use super::super::validate_addrs;
 use crate::types::MetaAddr;
@@ -9,10 +10,9 @@ proptest! {
     #[test]
     fn no_last_seen_times_are_in_the_future(
         gossiped_peers in vec(MetaAddr::gossiped_strategy(), 1..10),
+        last_seen_limit in datetime_full(),
     ) {
         zebra_test::init();
-
-        let last_seen_limit = Utc::now();
 
         let validated_peers = validate_addrs(gossiped_peers, last_seen_limit);
 
