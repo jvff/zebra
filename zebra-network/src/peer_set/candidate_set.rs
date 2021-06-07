cbf4b3af3c134e7a954431ee7eeef711bc6a42cb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-07 10:23:42 -0300

    Mention that call to `update` may be skipped
    Make it clearer that in this case the rate limiting causes calls to be
    skipped, and not that there's an internal sleep that happens.
    
    Also remove "to the same peers", because it's more general than that.
    
    Co-authored-by: teor <teor@riseup.net>

--- zebra-network/src/peer_set/candidate_set.rs
+++ zebra-network/src/peer_set/candidate_set.rs
@@ -158,9 +158,9 @@ where
     /// ## Security
     ///
     /// This call is rate-limited to prevent sending a burst of repeated requests for new peer
-    /// addresses to the same peers. Each call will only update the [`CandidateSet`] if more time
+    /// addresses. Each call will only update the [`CandidateSet`] if more time
     /// than [`MIN_PEER_GET_ADDR_INTERVAL`][Self::MIN_PEER_GET_ADDR_INTERVAL] has passed since the
-    /// last call.
+    /// last call. Otherwise, the update is skipped.
     pub async fn update(&mut self) -> Result<(), BoxError> {
         if self.next_update_time <= Instant::now() {
             self.update_timeout(None).await?;
