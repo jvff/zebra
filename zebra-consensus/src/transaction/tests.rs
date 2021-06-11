0a91ae5740c09600d28c82bfc405499a94d24c2a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-11 21:54:21 +0000

    Test that V5 transaction is rejected on Canopy
    Create a fake V5 transaction and try to verify it using a block height
    from Canopy's activation. The verifier should reject the transaction
    with an error saying that the network upgrade does not support that
    transaction version.

--- zebra-consensus/src/transaction/tests.rs
+++ zebra-consensus/src/transaction/tests.rs
@@ -1,15 +1,19 @@
+use std::{collections::HashMap, sync::Arc};
+
+use tower::{service_fn, ServiceExt};
+
 use zebra_chain::{
     orchard,
-    parameters::Network,
+    parameters::{Network, NetworkUpgrade},
     transaction::{
         arbitrary::{fake_v5_transactions_for_network, insert_fake_orchard_shielded_data},
         Transaction,
     },
 };
 
-use super::check;
+use super::{check, Request, Verifier};
 
-use crate::error::TransactionError;
+use crate::{error::TransactionError, script};
 use color_eyre::eyre::Report;
 
 #[test]
@@ -158,5 +162,35 @@ fn v5_coinbase_transaction_with_enable_spends_flag_fails_validation() {
     assert_eq!(
         check::coinbase_tx_no_prevout_joinsplit_spend(&transaction),
         Err(TransactionError::CoinbaseHasEnableSpendsOrchard)
+    );
+}
+
+#[tokio::test]
+async fn v5_transaction_is_rejected_before_nu5_activation() {
+    let network = Network::Mainnet;
+    let blocks = zebra_test::vectors::MAINNET_BLOCKS.iter();
+    let canopy = NetworkUpgrade::Canopy;
+
+    let state_service = service_fn(|_| async { unreachable!("Service should not be called") });
+    let script_verifier = script::Verifier::new(state_service);
+    let verifier = Verifier::new(network, script_verifier);
+
+    let transaction = fake_v5_transactions_for_network(network, blocks)
+        .next()
+        .expect("At least one fake V5 transaction in the test vectors");
+
+    let result = verifier
+        .oneshot(Request::Block {
+            transaction: Arc::new(transaction),
+            known_utxos: Arc::new(HashMap::new()),
+            height: canopy
+                .activation_height(network)
+                .expect("Canopy activation height is specified"),
+        })
+        .await;
+
+    assert_eq!(
+        result,
+        Err(TransactionError::UnsupportedByNetworkUpgrade(5, canopy))
     );
 }
