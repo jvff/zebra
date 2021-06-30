433ff654c1acfb01e5d6d315f0cb7addccd9cdfa
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-30 01:30:37 +0000

    Test V4 tx. with Sapling outputs but no spends
    Find a transaction V4 vector that has Sapling outputs but no spends, and
    check that the verifier accepts it.

--- zebra-consensus/src/transaction/tests.rs
+++ zebra-consensus/src/transaction/tests.rs
@@ -732,6 +732,43 @@ async fn v4_with_sapling_spends() {
     assert_eq!(result, Ok(expected_hash));
 }
 
+/// Test if a V4 transaction with Sapling outputs but no spends is accepted by the verifier.
+#[tokio::test]
+async fn v4_with_sapling_outputs_and_no_spends() {
+    zebra_test::init();
+
+    let network = Network::Mainnet;
+
+    let (height, transaction) = test_transactions(network)
+        .rev()
+        .filter(|(_, transaction)| !transaction.is_coinbase() && transaction.inputs().is_empty())
+        .find(|(_, transaction)| {
+            transaction.sapling_spends_per_anchor().next().is_none()
+                && transaction.sapling_outputs().next().is_some()
+        })
+        .expect("No transaction found with Sapling spends");
+
+    let expected_hash = transaction.hash();
+
+    // Initialize the verifier
+    let state_service =
+        service_fn(|_| async { unreachable!("State service should not be called") });
+    let script_verifier = script::Verifier::new(state_service);
+    let verifier = Verifier::new(network, script_verifier);
+
+    // Test the transaction verifier
+    let result = verifier
+        .clone()
+        .oneshot(Request::Block {
+            transaction,
+            known_utxos: Arc::new(HashMap::new()),
+            height,
+        })
+        .await;
+
+    assert_eq!(result, Ok(expected_hash));
+}
+
 // Utility functions
 
 /// Create a mock transparent transfer to be included in a transaction.
