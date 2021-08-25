8c0bdeddba35d6be92d9f45f51b1e1c7208bd86c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-25 01:31:25 +0000

    Create a `mempool::is_enabled` stub function
    Will be implemented in a separate PR.

--- zebrad/src/components/mempool.rs
+++ zebrad/src/components/mempool.rs
@@ -3,3 +3,8 @@
 mod crawler;
 
 pub use self::crawler::Crawler;
+
+fn is_enabled(_latest_sync_height: &[usize]) -> bool {
+    // TODO: Check if synchronizing up to chain tip has finished (#2592).
+    true
+}
