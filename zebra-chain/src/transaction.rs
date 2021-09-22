e43a2384c0c70c4f26b1b9ae395e19d832cb0ba3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-09-22 19:49:44 -0300

    Clarify that the outpoints are from inputs
    Because otherwise it could lead to confusion because it could also mean
    the outputs of the transaction represented as `OutPoint` references.
    
    Co-authored-by: teor <teor@riseup.net>

--- zebra-chain/src/transaction.rs
+++ zebra-chain/src/transaction.rs
@@ -343,7 +343,7 @@ impl Transaction {
         }
     }
 
-    /// Access the [`transparent::OutPoint`]s spent by this transaction.
+    /// Access the [`transparent::OutPoint`]s spent by this transaction's [`transparent::Input`]s.
     pub fn spent_outpoints(&self) -> impl Iterator<Item = transparent::OutPoint> + '_ {
         self.inputs()
             .iter()
