d531a90ade8d4accb5fce25634382ceb9583bbd4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-10-20 14:19:21 +0000

    Replace usage of `ServiceExt::ready_and`
    It was deprecated in favor of `ServiceExt::ready`.

--- tower-batch/src/worker.rs
+++ tower-batch/src/worker.rs
@@ -84,7 +84,7 @@ where
             tracing::trace!("notifying caller about worker failure");
             let _ = tx.send(Err(failed.clone()));
         } else {
-            match self.service.ready_and().await {
+            match self.service.ready().await {
                 Ok(svc) => {
                     let rsp = svc.call(req.into());
                     let _ = tx.send(Ok(rsp));
@@ -110,7 +110,7 @@ where
     async fn flush_service(&mut self) {
         if let Err(e) = self
             .service
-            .ready_and()
+            .ready()
             .and_then(|svc| svc.call(BatchControl::Flush))
             .await
         {
