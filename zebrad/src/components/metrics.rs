d4b9eff72702509c781322d06e5e7f0a6e203dd8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-10-29 13:42:31 +0000

    fixup! Simplify build version counter

--- zebrad/src/components/metrics.rs
+++ zebrad/src/components/metrics.rs
@@ -23,7 +23,10 @@ impl MetricsEndpoint {
                     // Expose binary metadata to metrics, using a single time series with
                     // value 1:
                     //     https://www.robustperception.io/exposing-the-software-version-to-prometheus
-                    metrics::increment_counter!(format!("{}.build.info", env!("CARGO_PKG_NAME")));
+                    metrics::increment_counter!(
+                        format!("{}.build.info", env!("CARGO_PKG_NAME")),
+                        "version" => env!("CARGO_PKG_VERSION")
+                    );
                 }
                 Err(e) => panic!(
                     "Opening metrics endpoint listener {:?} failed: {:?}. \
