ded5981d256a221096eaf22d7f3e8d1528118fb0
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-11-15 19:44:11 +0000

    Validate transaction lock times
    If they are enabled by a transaction's transparent input sequence
    numbers, make sure that they are in the past.

--- zebra-consensus/src/transaction.rs
+++ zebra-consensus/src/transaction.rs
@@ -302,6 +302,7 @@ where
             let (utxo_sender, mut utxo_receiver) = mpsc::unbounded_channel();
 
             // Do basic checks first
+            check::lock_time_has_passed(&tx, req.height(), req.block_time())?;
             check::has_inputs_and_outputs(&tx)?;
             check::has_enough_orchard_flags(&tx)?;
 
