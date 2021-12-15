3a8d9ebd6ac5198fd379b17888c13c4ae8728347
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-12-15 23:54:35 +0000

    Add `IsReady` extension trait for `Service`
    Simplifies checking if a service is immediately ready to be called.

---
+++ zebra-test/src/service_extensions.rs
@@ -0,0 +1,19 @@
+//! Extension traits for [`Service`] types to help with testing.
+
+use futures::FutureExt;
+use tower::{Service, ServiceExt};
+
+/// An extension trait to check if a [`Service`] is immediately ready to be called.
+pub trait IsReady<Request>: Service<Request> {
+    /// Check if the [`Service`] is immediately ready to be called.
+    fn is_ready(&mut self) -> bool;
+}
+
+impl<S, Request> IsReady<Request> for S
+where
+    S: Service<Request>,
+{
+    fn is_ready(&mut self) -> bool {
+        matches!(self.ready().now_or_never(), Some(Ok(_)))
+    }
+}
