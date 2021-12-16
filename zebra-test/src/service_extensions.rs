7b3bda33d0b08612fea8ede1a8e256cfcb740931
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-12-16 13:21:46 +0000

    Replace `now_or_never` with `NowOrLater`
    Avoid hiding a potentially dangerous call to `now_or_never`.

--- zebra-test/src/service_extensions.rs
+++ zebra-test/src/service_extensions.rs
@@ -1,26 +1,33 @@
 //! Extension traits for [`Service`] types to help with testing.
 
-use futures::FutureExt;
+use futures::future::{BoxFuture, FutureExt};
 use tower::{Service, ServiceExt};
+
+use now_or_later::NowOrLater;
 
 /// An extension trait to check if a [`Service`] is immediately ready to be called.
 pub trait IsReady<Request>: Service<Request> {
     /// Check if the [`Service`] is immediately ready to be called.
-    fn is_ready(&mut self) -> bool;
+    fn is_ready(&mut self) -> BoxFuture<bool>;
 
     /// Check if the [`Service`] is not immediately ready because it returns an error.
-    fn not_ready_due_to_error(&mut self) -> bool;
+    fn not_ready_due_to_error(&mut self) -> BoxFuture<bool>;
 }
 
 impl<S, Request> IsReady<Request> for S
 where
-    S: Service<Request>,
+    S: Service<Request> + Send,
+    Request: 'static,
 {
-    fn is_ready(&mut self) -> bool {
-        matches!(self.ready().now_or_never(), Some(Ok(_)))
+    fn is_ready(&mut self) -> BoxFuture<bool> {
+        NowOrLater(self.ready())
+            .map(|ready_result| matches!(ready_result, Some(Ok(_))))
+            .boxed()
     }
 
-    fn not_ready_due_to_error(&mut self) -> bool {
-        matches!(self.ready().now_or_never(), Some(Err(_)))
+    fn not_ready_due_to_error(&mut self) -> BoxFuture<bool> {
+        NowOrLater(self.ready())
+            .map(|ready_result| matches!(ready_result, Some(Err(_))))
+            .boxed()
     }
 }
