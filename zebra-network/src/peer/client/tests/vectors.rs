86ff15327f964e6caa93eb7887aa4cac6cacc2aa
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-12-17 02:13:58 +0000

    Test if `Client` handles panics in connection task
    Use a mock background connection task that panics immediately, and check
    that the `Client` handles it gracefully.

--- zebra-network/src/peer/client/tests/vectors.rs
+++ zebra-network/src/peer/client/tests/vectors.rs
@@ -116,6 +116,26 @@ async fn client_service_handles_exited_connection_task() {
     assert!(harness.try_to_receive_outbound_client_request().is_closed());
 }
 
+/// Force the connection background task to panic, and check if the `Client` properly handles it.
+#[tokio::test]
+async fn client_service_handles_panicked_connection_task() {
+    zebra_test::init();
+
+    let (mut client, mut harness) = ClientTestHarness::build()
+        .with_connection_task(async move {
+            panic!("connection task failure");
+        })
+        .finish();
+
+    // Allow the custom connection task to run.
+    tokio::task::yield_now().await;
+
+    assert!(client.is_failed().await);
+    assert!(harness.current_error().is_some());
+    assert!(!harness.wants_connection_heartbeats());
+    assert!(harness.try_to_receive_outbound_client_request().is_closed());
+}
+
 /// Force the heartbeat background task to stop, and check if the `Client` properly handles it.
 #[tokio::test]
 async fn client_service_handles_exited_heartbeat_task() {
