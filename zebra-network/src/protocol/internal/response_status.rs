0cd3d6da03ab7ee383219232b2036823d312a4ec
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-02-13 02:39:03 +0000

    Remove some dead code attributes
    They are not necessary.

--- zebra-network/src/protocol/internal/response_status.rs
+++ zebra-network/src/protocol/internal/response_status.rs
@@ -37,20 +37,17 @@ impl<A, M> ResponseStatus<A, M> {
     }
 
     /// Returns true if the inventory item was available.
-    #[allow(dead_code)]
     pub fn is_available(&self) -> bool {
         matches!(self, Available(_))
     }
 
     /// Returns true if the inventory item was missing.
-    #[allow(dead_code)]
     pub fn is_missing(&self) -> bool {
         matches!(self, Missing(_))
     }
 
     /// Maps a `ResponseStatus<A, M>` to `ResponseStatus<B, M>` by applying a function to a
     /// contained [`Available`] value, leaving the [`Missing`] value untouched.
-    #[allow(dead_code)]
     pub fn map_available<B, F: FnOnce(A) -> B>(self, f: F) -> ResponseStatus<B, M> {
         // Based on Result::map from https://doc.rust-lang.org/src/core/result.rs.html#765
         match self {
@@ -61,7 +58,6 @@ impl<A, M> ResponseStatus<A, M> {
 
     /// Maps a `ResponseStatus<A, M>` to `ResponseStatus<A, N>` by applying a function to a
     /// contained [`Missing`] value, leaving the [`Available`] value untouched.
-    #[allow(dead_code)]
     pub fn map_missing<N, F: FnOnce(M) -> N>(self, f: F) -> ResponseStatus<A, N> {
         // Based on Result::map_err from https://doc.rust-lang.org/src/core/result.rs.html#850
         match self {
@@ -90,7 +86,6 @@ impl<A: Clone, M: Clone> ResponseStatus<A, M> {
     }
 
     /// Get the missing inventory item, if present.
-    #[allow(dead_code)]
     pub fn missing(&self) -> Option<M> {
         if let Missing(item) = self {
             Some(item.clone())
